3446200fc45592aa3e4b9ec1d1266405f07343ef
Sallen54 <alejandalmau@gmail.com>
2024-02-05 17:56:35 +0100

    me he adelantado a la actividad

--- bin/dart1.dart
+++ bin/dart1.dart
@@ -1,11 +1,6 @@
 import 'package:dart1/dart1.dart' as dart1;
 
 void main(List<String> arguments) {
-    // Obtener la fecha y hora actual
-    DateTime now = DateTime.now();
-    // Formatear la fecha y hora
-    String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
-    // Imprimir el mensaje con la fecha y hora
-    print('Hello world: ${dart1.calculate()}! ($formattedDateTime)');
+  print('Hello world: ${dart1.calculate()}!');
 }
 
